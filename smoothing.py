221c1fa0ea3c640af650cb9a918ab98cf48a2f31
Daniel Salgado Rojo <daniel.salgado.rojo@gmail.com>
2018-01-13 19:08:16 +0100

    Add cellular_automata_smoothing function to try to reproduce a model from a paper but it does not work properly yet

--- smoothing.py
+++ smoothing.py
@@ -19,3 +19,38 @@ def slope_smoothing(road: Road):
         elif diff < -1:
             road.add_grain(i)
             road.remove_grain(i2)
+
+def cellular_automata_smoothing(road: Road, iterations: int,
+                                D: float, gamma: float,
+                                v: float, S_c: int, d: int ):
+    #FOR US d = 1? width of columns
+
+    N = road.size
+    R = road.piles.astype(float)
+    h = np.maximum(R-2,0)
+
+    #h = road.piles.astype(float)
+    #R = np.maximum(h-2,0)
+
+    dR = np.full(N, 0, dtype=np.float)
+    dh = np.full(N, 0, dtype=np.float)
+
+    for i in range(N):
+        # im = (i-1) % N
+        ip = (i + 1) % N
+        dh[i] = (h[ip] - h[i]) / d
+        dR[i] = (R[ip] - R[i]) / d
+
+    for iter in range(iterations):
+        for i in range(N):
+            #im = (i-1) % N
+            ip = (i+1) % N
+
+            dh[i] = (h[ip]-h[i])/d
+            dR[i] = (R[ip] - R[i])/d
+
+            R[i] = R[i] + (-v*R[i] + D*dR[i]) - R[i]*gamma*(dh[i]-abs(S_c))
+            h[i] = h[i] + R[i]*gamma*(dh[i] - abs(S_c))
+            R[ip] = R[ip] - (-v*R[i] + D*dR[i])
+
+    road.piles = np.round(R).astype(int)
