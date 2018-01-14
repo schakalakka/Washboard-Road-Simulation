9775b6aeb538f3923cc61a52b79734056de7b565
Daniel Salgado Rojo <daniel.salgado.rojo@gmail.com>
2018-01-14 10:28:08 +0100

    Add quadratic and exponential probabilities for the digg_backwards_softness function

--- digging.py
+++ digging.py
@@ -37,6 +37,21 @@ def dig_backwards(road: Road, wheel: Wheel, position: int):  # -> Tuple[np.ndarr
     road.piles[remove_from] -= increments
     road.piles[put_on] += increments
 
+
+def quadratic_dig_probability(h: np.array, h0: int) -> np.array:
+    p = h
+    p[h < 0] = 0
+    p[(h>=0) | (h <= h0)] = p[(h>=0) | (h <= h0)]**3/h0**3
+    p[h>h0] = 1
+    return(p)
+
+def exponential_dig_probability(h: np.array, h0: int, alpha: float) -> np.array:
+    p = h
+    p[h < 0] = 0
+    p[(h>=0) | (h <= h0)] = (np.exp(alpha * p[(h>=0) | (h <= h0)]) -1)/ (np.exp(alpha * h0) -1)
+    p[h > h0] = 1
+    return(p)
+
 def dig_backwards_softness(road: Road, wheel: Wheel, position: int):  # -> Tuple[np.ndarray, int]:
     """
     @TODO
@@ -48,7 +63,13 @@ def dig_backwards_softness(road: Road, wheel: Wheel, position: int):  # -> Tuple
     """
     remove_from = np.mod(np.arange(position - wheel.diameter + 1, position + 1), road.size)
     put_on = np.mod(np.arange(position - 2 * wheel.diameter + 1, position - wheel.diameter + 1), road.size)
-    increments = (road.piles[remove_from] > 0).astype(int)
+
+    random_probs = np.random.uniform(0, 1, wheel.diameter)
+    #dig_probs = quadratic_dig_probability(road.piles[remove_from], road.height)
+    alpha = 0.005
+    dig_probs = exponential_dig_probability(road.piles[remove_from], road.height, alpha)
+
+    increments = (random_probs <= dig_probs).astype(int)
 
     if len(remove_from) != len(put_on):
         print("\nWe are going to remove or put more or less than put or remove grains\n")
