0e5d81db462edb47f5d5cfbefbc70075f4788349
Daniel Salgado Rojo <daniel.salgado.rojo@gmail.com>
2018-01-14 21:45:51 +0100

    Add bump.py to contain functions related to bumps

--- main.py
+++ main.py
@@ -8,45 +8,7 @@ import sys
 from digging import *
 from smoothing import *
 from utils import *
-
-
-
-def determine_bump_height(road: Road, wheel: Wheel, position: int, method='max') -> int:
-    """
-    General bump height function, returns the bump height of a position on a road for a given
-    wheel_size and method
-    :param road: a Road
-    :param wheel: a Wheel
-    :param position: int
-    :param method: str, 'max'
-    :return: a bump_height function call
-    """
-    available_methods = list(['max', 'max'])
-    default_method = 'max'
-    method = method
-
-    if method not in available_methods:
-        print("The bump_height method '", method, "' does not exist. Using default method '", default_method, "'")
-        method = 'max'
-
-    if method == 'max':
-        return max_bump_height(road, wheel, position)
-
-
-def max_bump_height(road: Road, wheel: Wheel, position: int) -> int:
-    """
-    At position i determine the bump height of the next w elements/indexes/positions
-    with w being the wheel diameter.
-    The bump height is determined by taking the maximum from the positions i+1 to i+w (inclusive) minus
-    the current elevation of the wheel.
-    :param road: a Road
-    :param wheel: a Wheel
-    :param position: int, current (front) position of the wheel
-                         (it should be the first position before the bump position)
-    :return: the bump height
-    """
-    return np.max(road.piles[(position + 1): (position + 1 + wheel.diameter)]) - wheel.elevation
-
+from bump import *
 
 ####wheel
 def move_to_next_bump(road: Road, wheel: Wheel) -> int:
